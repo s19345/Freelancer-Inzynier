7864796cf57e1ec56ac1209e1e27b39c7af2960d
Marlena <s19345@pjwstk.edu.pl>
2025-05-26 21:03:51 +0200

    add router and register UserViewSet in users/urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
-from .views import CreateUserView, UserListView
+from .views import CreateUserView, UserListView, UserViewSet
+from rest_framework.routers import DefaultRouter
+
+
+user_router = DefaultRouter()
+user_router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('create_user/', CreateUserView.as_view(), name='create_user'),
     path('users/', UserListView.as_view(), name='user_list'),
+    path('', include(user_router.urls), name='user_view_set')
 ]
