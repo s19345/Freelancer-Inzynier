bae574b55f768cd3b1d9c399f2dd328ff0806325
Marlena <s19345@pjwstk.edu.pl>
2025-08-04 11:56:27 +0200

    updated backend address

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,4 +1,4 @@
-export const BACKEND_HOST = "http://127.0.0.1:8000"
+export const BACKEND_HOST = "https://freelancer-dgnm.onrender.com"
 
 export const USERS_BACKEND_URL = `${BACKEND_HOST}/users/auth/`;
 export const PROJECT_BACKEND_URL = `${BACKEND_HOST}/project/`;
